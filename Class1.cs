b008b219f0282cb44109c2a0a2b83ec67b2f8826
DaikoGames <136380205+DaikoGames@users.noreply.github.com>
2026-09-02 11:08:46 +0200

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,4 @@
-﻿using CliWrap;
+using CliWrap;
 using System.Diagnostics;
 using GnomeStack.OS.Release;
 using System.Threading.Tasks;
@@ -7,44 +7,58 @@ namespace LibVLC.Linux
 {
     public class LibVLCLinux
     {
-        public async void InstallVLC()
+        public async Task InstallVLC()
         {
             try
             {
-                var OperatingSystemName = OsRelease.Current.Id?.ToLowerInvariant();
+                var operatingSystemName = OsRelease.Current.Id?.ToLowerInvariant() ?? string.Empty;
+                var idLike = OsRelease.Current.IdLike?.ToLowerInvariant() ?? string.Empty;
 
-                if (OperatingSystemName == "debian" || OperatingSystemName == "ubuntu" || OperatingSystemName == "linuxmint")
+                Trace.WriteLine($"LibVLC.Linux - ID={operatingSystemName} ID_LIKE={idLike}");
+
+                string command = null;
+
+                if (operatingSystemName == "debian" || operatingSystemName == "ubuntu" || operatingSystemName == "linuxmint"
+                    || idLike.Contains("debian") || idLike.Contains("ubuntu"))
                 {
-
-                    await Cli.Wrap("pkexec").WithArguments($"bash -c \"apt update && apt install -y vlc libvlc-dev\"").ExecuteAsync();
+                    command = "apt-get update && apt-get install -y vlc libvlc-dev";
+                }
+                else if (operatingSystemName == "opensuse" || operatingSystemName == "opensuse-leap" || operatingSystemName == "opensuse-tumbleweed"
+                         || idLike.Contains("suse"))
+                {
+                    command = "zypper --non-interactive install vlc libvlc-devel";
+                }
+                else if (operatingSystemName == "gentoo")
+                {
+                    command = "emerge -av vlc";
+                }
+                else if (operatingSystemName == "fedora" || idLike.Contains("fedora"))
+                {
+                    command = "dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm vlc libvlc-devel";
+                }
+                else if (operatingSystemName == "arch" || operatingSystemName == "manjaro" || idLike.Contains("arch"))
+                {
+                    command = "pacman -Sy --noconfirm vlc";
+                }
+                else if (operatingSystemName == "rhel" || operatingSystemName == "almalinux" || operatingSystemName == "rocky"
+                         || idLike.Contains("rhel"))
+                {
+                    command = "yum install -y epel-release https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm vlc libvlc-devel";
                 }
 
-                if (OperatingSystemName == "opensuse" || OperatingSystemName == "opensuse-leap" || OperatingSystemName == "opensuse-tumbleweed")
+                if (string.IsNullOrWhiteSpace(command))
                 {
-                    await Cli.Wrap("pkexec").WithArguments($"bash -c \"zypper install -y vlc libvlc-devel\"").ExecuteAsync();
+                    throw new Exception($"No install command for distro ID='{operatingSystemName}' ID_LIKE='{idLike}'.");
                 }
 
-                if (OperatingSystemName == "gentoo")
-                {
-                    await Cli.Wrap("pkexec").WithArguments($"bash -c \"emerge -av vlc\"").ExecuteAsync();
-                }
-
-                if (OperatingSystemName == "fedora")
-                {
-                    string fedoraCmd = "dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm vlc libvlc-devel";
-                    await Cli.Wrap("pkexec").WithArguments($"bash -c \"{fedoraCmd}\"").ExecuteAsync();
-                }
-
-                if (OperatingSystemName == "arch" || OperatingSystemName == "manjaro")
-                {
-                    await Cli.Wrap("pkexec").WithArguments($"bash -c \"pacman -S --noconfirm vlc\"").ExecuteAsync();
-                }
-
-                if (OperatingSystemName == "rhel" || OperatingSystemName == "almalinux" || OperatingSystemName == "rocky")
-                {
-                    string rhelCmd = "yum install -y epel-release https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm vlc libvlc-devel";
-                    await Cli.Wrap("pkexec").WithArguments($"bash -c \"{rhelCmd}\"").ExecuteAsync();
-                }
+                await Cli.Wrap("/usr/bin/pkexec")
+                    .WithArguments(new[]
+                    {
+                        "/usr/bin/bash",
+                        "-c",
+                        command
+                    })
+                    .ExecuteAsync();
             }
             catch (Exception ex)
             {
@@ -52,6 +66,7 @@ namespace LibVLC.Linux
                 Trace.WriteLine("Either you wanted to run this on a non-Linux OS, the Linux Distro you chose does not exist for VLC, or you wrote the Distro name wrong.");
                 Trace.WriteLine("Distro IDs are: debian, ubuntu, linuxmint, opensuse, gentoo, fedora, arch, rhel");
                 Trace.WriteLine($"Exception Details: {ex.Message}");
+                Trace.WriteLine(ex.ToString());
                 Trace.WriteLine("Another possibility is that I wrote the code wrong, if that is the case, please report the issue on Github. We can fix it :).");
             }
         }
